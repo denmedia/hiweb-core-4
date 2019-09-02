4bf359d02ce8e5e9911a5bccb46492e5c35773f8
DenMedia <d9251132826@gmail.com>
2019-09-02 10:49:30 +0300

    4.0.0.0

---
+++ hiweb-core-4.php
@@ -0,0 +1,18 @@
+<?php
+	/*
+	Plugin Name: hiWeb Core 4
+	Plugin URI: http://plugins.hiweb.moscow/core
+	Description: Framework Plugin for WordPress min v4.8
+	Version: 4.0.0.0
+	Author: Den Media
+	Author URI: http://hiweb.moscow
+	*/
+
+	if( version_compare( PHP_VERSION, '7.0.0' ) >= 0 ){
+//	    require_once __DIR__ . '/include/spl_autoload_register.php';
+//		require_once __DIR__ . '/traits/hidden_methods.php';
+//		require_once __DIR__ . '/include/define.php';
+//		require_once __DIR__ . '/include/init.php';
+	} else {
+		//TODO: показать ошибку версии PHP!
+	}
